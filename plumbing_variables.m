c8bc8c32c586c67518e1141feb66df02d4b421b3
Solan T Israel Megerssa <solan@mit.edu>
2018-03-20 22:05:35 -0400

    added wall plumbing model

---
+++ plumbing_variables.m
@@ -0,0 +1,13 @@
+%% Parameters of interest for WALL water system
+
+swh_valve_open = 0;
+cistern_valve_open = 1;
+
+% cistern properties
+roof_cistern_pipe_length = 10; % feet
+cistern_inlet_diameter = 2; % inches
+
+cistern_outlet_diameter = 2; % inches
+cistern_pump_pipe_length = 1; % feet
+
+pump_inlet_area = 3.14*1^2; % inches^2
