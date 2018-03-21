78dc5e35250e3824e0677f2f884eebce5968481a
Solan T Israel Megerssa <solan@mac-w20-575-09.mit.edu>
2018-03-20 23:15:59 -0400

    updated plumbing to low pressure hydraulics

--- plumbing_variables.m
+++ plumbing_variables.m
@@ -1,13 +1,22 @@
 %% Parameters of interest for WALL water system
 
+
+% roof properties
+roof_cistern_pipe_length = 10; % feet
+roof_height = 15; % feet
+
+% swh heater properties
 swh_valve_open = 0;
-cistern_valve_open = 1;
+swh_volume = 2500; % gallons
+swh_inlet_diam = 2; % inches
+swh_outlet_diam = 2; % inches
 
 % cistern properties
-roof_cistern_pipe_length = 10; % feet
-cistern_inlet_diameter = 2; % inches
-
-cistern_outlet_diameter = 2; % inches
+roof_valve_open = 1;
+cistern_inlet_height = 5; % feet
+cistern_diam = 100; % inches
+cistern_inlet_diam = 2; % inches
+cistern_outlet_diam = 2; % inches
 cistern_pump_pipe_length = 1; % feet
 
 pump_inlet_area = 3.14*1^2; % inches^2
